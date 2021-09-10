5a46a0dce791c90bee04819528974e9789dbdaa2
dqwdd <dqwdd@naver.com>
2021-09-10 11:46:16 +0900

    로그인화면 - KeyHash값 추출 - 페이스북 라이브러리 설치 진행중

--- app/src/main/java/com/example/databinding20210910/LoginActivity.kt
+++ app/src/main/java/com/example/databinding20210910/LoginActivity.kt
@@ -1,8 +1,12 @@
 package com.example.databinding20210910
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.databinding20210910.databinding.ActivityLoginBinding
+import java.security.MessageDigest
 
 class LoginActivity : BaseActivity() {
 
@@ -20,5 +24,18 @@ class LoginActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        카톡으로 받은 코드 복붙 => 키 해시값 추출
+
+        val info = packageManager.getPackageInfo(
+            "com.example.databinding20210910",
+            PackageManager.GET_SIGNATURES
+        )
+        for (signature in info.signatures) {
+            val md: MessageDigest = MessageDigest.getInstance("SHA")
+            md.update(signature.toByteArray())
+            Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+        }
+
     }
 }

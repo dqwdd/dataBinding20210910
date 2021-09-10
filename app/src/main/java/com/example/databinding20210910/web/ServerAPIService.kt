7d4baa08af01f5c3519b1446b9510851d814d902
dqwdd <dqwdd@naver.com>
2021-09-10 16:04:39 +0900

    API 엔드 포인트를 명시하는 인터페이스 추가 - 회원가입 API 사용 예시

---
+++ app/src/main/java/com/example/databinding20210910/web/ServerAPIService.kt
@@ -0,0 +1,17 @@
+package com.example.databinding20210910.web
+
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.PUT
+
+interface ServerAPIService {
+
+
+    @FormUrlEncoded
+    @PUT("/user")//어느 주소로 갈거냐
+    fun putRequestSignUp(@Field("email") email: String,
+                         @Field("password") pw: String,
+                         @Field("nick_name") nickname: String)//폼데이터의 email에 뒤에 데이터들 담아주세요
+
+
+}

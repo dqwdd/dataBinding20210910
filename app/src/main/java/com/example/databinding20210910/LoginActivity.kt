dbe9f9a963f5ec74e38b8d39f15960defef9e166
dqwdd <dqwdd@naver.com>
2021-09-10 11:59:21 +0900

    페북로그인버튼활용 - 로그인처리

--- app/src/main/java/com/example/databinding20210910/LoginActivity.kt
+++ app/src/main/java/com/example/databinding20210910/LoginActivity.kt
@@ -1,26 +1,67 @@
 package com.example.databinding20210910
 
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Base64
 import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.databinding20210910.databinding.ActivityLoginBinding
+import com.facebook.CallbackManager
 import java.security.MessageDigest
+import com.facebook.FacebookException
+
+import com.facebook.login.LoginResult
+
+import com.facebook.FacebookCallback
+
+import android.view.View
+
+import com.facebook.login.widget.LoginButton
+
+
+
 
 class LoginActivity : BaseActivity() {
+
+    lateinit var callbackManager: CallbackManager
 
     lateinit var binding : ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
         setupEvent()
         setValues()
     }
 
     override fun setupEvent() {
+
+        callbackManager = CallbackManager.Factory.create();
+
+        binding.loginButton.setReadPermissions("email")
+
+        // Callback registration
+
+        // Callback registration
+        binding.loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+            override fun onSuccess(loginResult: LoginResult?) {
+                // App code
+
+                Log.d("확인용", loginResult.toString())
+            }
+
+            override fun onCancel() {
+                // App code
+            }
+
+            override fun onError(exception: FacebookException) {
+                // App code
+            }
+        })
+
+
     }
 
     override fun setValues() {
@@ -35,6 +76,12 @@ class LoginActivity : BaseActivity() {
             val md: MessageDigest = MessageDigest.getInstance("SHA")
             md.update(signature.toByteArray())
             Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+        }
+
+
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            callbackManager.onActivityResult(requestCode,resultCode,data)
+            super.onActivityResult(requestCode,resultCode,data)
         }
 
     }

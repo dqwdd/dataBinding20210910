e9c158cb0e0ba09b68fb978305b4a8e0d963991f
dqwdd <dqwdd@naver.com>
2021-09-13 17:10:48 +0900

    로딩화면에서 => 내 정보를 넣어서 메인으로 가야함.   - 내 정보를 어떻게?   - 토큰만 들고 있다.

---
+++ app/src/main/java/com/example/databinding20210910/SplashActivity.kt
@@ -0,0 +1,44 @@
+package com.example.databinding20210910
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.databinding20210910.utils.ContextUtil
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        setupEvent()
+        setValues()
+    }
+
+    override fun setupEvent() {
+    }
+
+    override fun setValues() {
+
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+
+            val myIntent: Intent
+
+            if (ContextUtil.getToken(mContext) != "") {
+
+//                GlobalData.loginUser = ?
+
+                myIntent = Intent(mContext, MainActivity::class.java)
+            } else {
+                myIntent = Intent(mContext, LoginActivity::class.java)
+            }
+
+            startActivity(myIntent)
+
+
+        }, 2500)
+
+    }
+}

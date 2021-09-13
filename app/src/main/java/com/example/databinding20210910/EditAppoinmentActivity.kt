28b580ae813c3a6c51ed5d3a8854ba9ce1189ecf
dqwdd <dqwdd@naver.com>
2021-09-13 12:13:03 +0900

    확인 버튼 누르면 일시 선택 여부 검증 + 최종적으로 서버에 보낼 문구 작성

--- app/src/main/java/com/example/databinding20210910/EditAppoinmentActivity.kt
+++ app/src/main/java/com/example/databinding20210910/EditAppoinmentActivity.kt
@@ -1,10 +1,13 @@
 package com.example.databinding20210910
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.DatePicker
 import android.widget.TimePicker
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.databinding20210910.databinding.ActivityEditAppoinmentBinding
 import java.sql.Time
@@ -17,7 +20,6 @@ class EditAppoinmentActivity : BaseActivity() {
 
 //    선택할 약속 일시를 저장할 변수
     val mSelectedDateTime = Calendar.getInstance()//기본값 = 현재 시간
-//    val mSelectedTime = Ti
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,6 +63,25 @@ class EditAppoinmentActivity : BaseActivity() {
 //            TimePicker띄우기 -> 입력 완료되면 시/분 제공
 //            mSelectedTime에 시/분 저장
 
+            val tsl = object  : TimePickerDialog.OnTimeSetListener {
+                override fun onTimeSet(p0: TimePicker?, hour: Int, minute: Int) {
+
+                    mSelectedDateTime.set(Calendar.HOUR_OF_DAY, hour)
+                    mSelectedDateTime.set(Calendar.MINUTE, minute)
+
+//                    오후 6:05  형태로 가공. => SimpleDateFormat
+                    val sdf = SimpleDateFormat("a h:mm")
+                    binding.timeTxt.text = sdf.format(mSelectedDateTime.time)
+
+                }
+
+            }
+
+            TimePickerDialog(mContext, tsl,
+                mSelectedDateTime.get(Calendar.HOUR_OF_DAY),
+                mSelectedDateTime.get(Calendar.MINUTE),
+                false).show()
+
         }
 
 
@@ -73,6 +94,24 @@ class EditAppoinmentActivity : BaseActivity() {
             val inputTitle = binding.titleEdt.text.toString()
 
 //            2.약속 일시 -> "2021-09-13 11:12" String 변환까지
+//             => 날짜 / 시간 중 선택 안한게 있다면? 선택하라고 토스트, 함수 강제 종료(validation)
+            if (binding.dateTxt.text == "약속 일자") {
+                Toast.makeText(mContext, "일자를 설정하지 않았습니다", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (binding.timeTxt.text == "약속 시간") {
+                Toast.makeText(mContext, "시간을 설정하지 않았습니다", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+//            여기 코드 실행 된다 : 일자 / 시간 모두 설정했다.
+//            선택된 약속일시를 -> "yyyy-MM-dd HH:mm"양식으로 가공(스웨거에 나온 양식) => 최종 서버에 파라미터로 첨부
+            val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm")
+            val finalDatetime = sdf.format(mSelectedDateTime.time)
+
+            Log.d("서버에 보낼 약속 일시", finalDatetime)
+
 
 
 //            3. 약속 장소?

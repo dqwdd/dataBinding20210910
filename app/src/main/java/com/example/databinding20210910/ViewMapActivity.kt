1cdc59758f8e3cf74dc2dd243c9453f903882a07
dqwdd <dqwdd@naver.com>
2021-09-14 11:10:03 +0900

    지도 보기 화면에서 받아낸 약속 정보를 갖고 -실제 지도 카메라 이동 -마커도 실제로 찍어주기

--- app/src/main/java/com/example/databinding20210910/ViewMapActivity.kt
+++ app/src/main/java/com/example/databinding20210910/ViewMapActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.databinding20210910.adapters.AppointmentAdapter
 import com.example.databinding20210910.datas.AppointmentData
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.overlay.Marker
 
 class ViewMapActivity : BaseActivity() {
 
@@ -22,6 +26,30 @@ class ViewMapActivity : BaseActivity() {
     override fun setValues() {
 
         mAppointmentData = intent.getSerializableExtra("appointment") as AppointmentData
+
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.naverMapFrag) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.naverMapFrag, it).commit()
+            }
+
+        mapFragment.getMapAsync {
+
+            val appointmentLatLng = LatLng(mAppointmentData.latitude, mAppointmentData.longitude)
+
+
+//            지도도 약속장소로 카메라를 옮겨주자
+            val cameraUpdate = CameraUpdate.scrollTo(appointmentLatLng)
+            it.moveCamera(cameraUpdate)
+
+            //마커를 찍고 표시
+
+            val marker = Marker()
+
+            marker.position = appointmentLatLng
+            marker.map = it
+
+        }
 
     }
 

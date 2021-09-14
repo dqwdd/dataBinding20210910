074b7039eddf59a4d8bbd7ac53c95dd53943abc7
dqwdd <dqwdd@naver.com>
2021-09-14 12:58:57 +0900

    ODSay 라이브러리 활용 => 대중교통 길찾기 -> 소요시간 (첫번쨰 검색 결과 활용)  => UI 텍스트뷰에 반영 X

--- app/src/main/java/com/example/databinding20210910/ViewMapActivity.kt
+++ app/src/main/java/com/example/databinding20210910/ViewMapActivity.kt
@@ -2,9 +2,11 @@ package com.example.databinding20210910
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import com.example.databinding20210910.adapters.AppointmentAdapter
 import com.example.databinding20210910.datas.AppointmentData
 import com.naver.maps.geometry.LatLng
@@ -12,6 +14,10 @@ import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.overlay.InfoWindow
 import com.naver.maps.map.overlay.Marker
+import com.odsay.odsayandroidsdk.API
+import com.odsay.odsayandroidsdk.ODsayData
+import com.odsay.odsayandroidsdk.ODsayService
+import com.odsay.odsayandroidsdk.OnResultCallbackListener
 
 class ViewMapActivity : BaseActivity() {
 
@@ -65,6 +71,56 @@ class ViewMapActivity : BaseActivity() {
 
                     placeNameTxt.text = mAppointmentData.placeName
                     arrivalTimeTxt.text = "??시간 ??분 소요 예정"//
+
+                    val myODSay = ODsayService.init(mContext, "1LXXwKUp0wg5d7YQ7MA9QAvDw59XNPYusjqx/nbI4to")
+
+                    myODSay.requestSearchPubTransPath(
+                        126.75315226866731.toString(), 37.50598420151453.toString(),
+                        mAppointmentData.longitude.toString(), mAppointmentData.latitude.toString(),
+                        null,null,null, object : OnResultCallbackListener{
+                            override fun onSuccess(p0: ODsayData?, p1: API?) {
+
+                                val jsonObj = p0!!.json
+                                val resultObj = jsonObj.getJSONObject("result")
+                                val pathArr = resultObj.getJSONArray("path")
+
+                                /*for ( i in 0 until pathArr.length()) {
+                                    val pathObj = pathArr.getJSONObject(i)
+                                    Log.d("API응답", pathObj.toString(4))
+                                }*/
+
+                                val firstPath = pathArr.getJSONObject(0)
+                                val infoObj = firstPath.getJSONObject("info")
+
+                                val totalTime = infoObj.getInt("totlaTime")
+
+                                Log.d("총 소요시간", totalTime.toString())
+//                                시간/분으로 분리 => 92분 -> 1시간 32분
+//                                시간 : 전체분 / 60
+//                                분 : 전체문 % 60
+
+                                val hour = totalTime/60
+                                val minute = totalTime%60
+
+                                Log.d("예상시간", hour.toString())
+                                Log.d("예상분", minute.toString())
+
+
+                                runOnUiThread {
+                                    arrivalTimeTxt.text = "${hour}시간 ${minute}분 소요 예상"
+                                }
+
+
+                            }
+
+                            override fun onError(p0: Int, p1: String?, p2: API?) {
+                                //실패 시 예상 시간 받아오지 못했다는 안내
+                                Log.d("예상시간 실패", p1!!)
+                                arrivalTimeTxt.text = "예상시간 받아오기 실패했습니다"
+
+
+                            }
+                        })
 
                     return myView
 

c8d31ab02bbadeccf32d4ac295ca960d6e04fdfe
dqwdd <dqwdd@naver.com>
2021-09-14 10:40:54 +0900

    약속장소 지도를 크게 보기 위한 UI 설정

---
+++ app/src/main/java/com/example/databinding20210910/ViewMapActivity.kt
@@ -0,0 +1,19 @@
+package com.example.databinding20210910
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewMapActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_map)
+        setValues()
+        setupEvent()
+    }
+
+    override fun setupEvent() {
+    }
+
+    override fun setValues() {
+    }
+}

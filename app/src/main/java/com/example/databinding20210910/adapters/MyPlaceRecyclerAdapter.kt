213ef3987f841f07e09090c26a6debb1740f9b28
dqwdd <dqwdd@naver.com>
2021-09-15 10:15:52 +0900

    어댑터의 뼈대 작업 진행중

---
+++ app/src/main/java/com/example/databinding20210910/adapters/MyPlaceRecyclerAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.databinding20210910.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.databinding20210910.R
+import com.example.databinding20210910.datas.PlaceData
+
+class MyPlaceRecyclerAdapter(
+    mContext: Context, val mList: List<PlaceData>) : RecyclerView.Adapter<MyPlaceRecyclerAdapter.MyViewHolder>() {
+
+    class MyViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+
+        val placeNameTxt = view.findViewById<TextView>(R.id.placeNameTxt)
+        val isPrimaryTxt = view.findViewById<TextView>(R.id.isPrimaryTxt)
+        val viewPlaceMapBtn = view.findViewById<ImageView>(R.id.viewPlaceMapBtn)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val view = LayoutInflater.from(mContext).inflate(R.layout.my_place_list_item, parent, false)
+        return MyViewHolder(view)
+
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+    }
+
+    override fun getItemCount() = mList.size
+
+}

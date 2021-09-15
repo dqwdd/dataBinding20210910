9d46fdb9886795ba1eaa23c2316c7c8a02410adc
dqwdd <dqwdd@naver.com>
2021-09-15 12:08:27 +0900

    Spinner 배치 + 어댑터 뼈대 작업  - 리스트뷰 사용법 그대로 진행중

---
+++ app/src/main/java/com/example/databinding20210910/adapters/StartPlaceSpinnerAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.databinding20210910.adapters
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
+import com.example.databinding20210910.R
+import com.example.databinding20210910.ViewMapActivity
+import com.example.databinding20210910.datas.AppointmentData
+import com.example.databinding20210910.datas.PlaceData
+import org.json.JSONObject
+import java.text.SimpleDateFormat
+
+class StartPlaceSpinnerAdapter(
+    val mContext : Context,
+    resId : Int,
+    val mList : List<PlaceData>) : ArrayAdapter<PlaceData>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var row = convertView
+        if (row == null) {
+            row = mInflater.inflate(R.layout.my_place_list_item, null)
+        }
+        row!!
+
+        val data = mList[position]
+
+
+
+        return row
+    }
+
+
+}

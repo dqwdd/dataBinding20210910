ae1199e647edecc4b70a6764c542150b76460dcb
dqwdd <dqwdd@naver.com>
2021-09-15 10:49:00 +0900

    뷰홀데 클래스에 데이터 세팅 함수 추가, 활용

--- app/src/main/java/com/example/databinding20210910/adapters/MyPlaceRecyclerAdapter.kt
+++ app/src/main/java/com/example/databinding20210910/adapters/MyPlaceRecyclerAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.databinding20210910.R
 import com.example.databinding20210910.datas.PlaceData
@@ -19,6 +20,25 @@ class MyPlaceRecyclerAdapter(
         val isPrimaryTxt = view.findViewById<TextView>(R.id.isPrimaryTxt)
         val viewPlaceMapBtn = view.findViewById<ImageView>(R.id.viewPlaceMapBtn)
 
+        fun setRealData( data: PlaceData ) {
+            placeNameTxt.text = data.name
+
+            if (data.isPrimary) {
+                isPrimaryTxt.visibility = View.VISIBLE
+            }
+            else {
+                isPrimaryTxt.visibility = View.GONE
+            }
+
+
+//            이벤트처리
+//            viewPlaceMapBtn.setOnClickListener {
+//                Toast.makeText(, "", Toast.LENGTH_SHORT).show()
+//            }
+
+
+        }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -30,12 +50,12 @@ class MyPlaceRecyclerAdapter(
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-
-//        실제 데이터를 뿌려주는 부분
-        val data = mList[position]
-
-//        예시 : 실제 장소 이름만 출력
-        holder.placeNameTxt.text = data.name
+//          위에 fun setRealData( data: PlaceData ) 만들어서 여기에 쓸 거 대신 넣어도 됨
+////        실제 데이터를 뿌려주는 부분
+//        val data = mList[position]
+//
+////        예시 : 실제 장소 이름만 출력
+//        holder.placeNameTxt.text = data.name
 
 
     }

e089905d91b8e7e2544765bd83c44ecd21602049
dqwdd <dqwdd@naver.com>
2021-09-16 12:43:31 +0900

    BaseFragment.kt에서 레트로핏 관련 변수도 물려주도록 함 -> 프래그먼트도, 액티비티처럼 API 호출 가능(apiService)

--- app/src/main/java/com/example/databinding20210910/fragments/BaseFragment.kt
+++ app/src/main/java/com/example/databinding20210910/fragments/BaseFragment.kt
@@ -3,14 +3,22 @@ package com.example.databinding20210910.fragments
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.example.databinding20210910.web.ServerAPI
+import com.example.databinding20210910.web.ServerAPIService
+import retrofit2.Retrofit
 
 abstract class BaseFragment : Fragment() {
 
     lateinit var mContext: Context
 
+    private lateinit var retrofit: Retrofit
+    lateinit var apiService : ServerAPIService
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         mContext = requireContext()
+        retrofit = ServerAPI.getRetrofit(mContext)
+        apiService = retrofit.create(ServerAPIService::class.java)
     }
 
     abstract fun setupEvents()

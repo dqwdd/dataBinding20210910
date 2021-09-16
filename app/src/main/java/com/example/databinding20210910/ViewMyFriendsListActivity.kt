06c2cc6b509452a2a8358b378f59f307dd1456e4
dqwdd <dqwdd@naver.com>
2021-09-16 12:19:39 +0900

    프래그먼트에 데이터바인딩처리도 같이 진행함

--- app/src/main/java/com/example/databinding20210910/ViewMyFriendsListActivity.kt
+++ app/src/main/java/com/example/databinding20210910/ViewMyFriendsListActivity.kt
@@ -4,14 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.databinding20210910.adapters.ViewMyFriendsListAdapter
+import com.example.databinding20210910.databinding.ActivityViewMyFriendsListBinding
 
 class ViewMyFriendsListActivity : BaseActivity() {
 
+    lateinit var binding : ActivityViewMyFriendsListBinding
     lateinit var vmfla : ViewMyFriendsListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=DataBindingUtil.setContentView(this, R.layout.activity_view_my_friends_list)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_view_my_friends_list)
         setupEvent()
         setValues()
     }
@@ -24,7 +26,7 @@ class ViewMyFriendsListActivity : BaseActivity() {
         vmfla = ViewMyFriendsListAdapter( supportFragmentManager )
         binding.friendsViewPager.adapter = vmfla
 
-        mainTabLayout.setupWithViewPager(mainViewPager)
+        friendsTabLayout.setupWithViewPager(friendsViewPager)
 
     }
 }
--- app/src/main/java/com/example/databinding20210910/fragments/MyFriendsListFragment.kt
+++ app/src/main/java/com/example/databinding20210910/fragments/MyFriendsListFragment.kt
@@ -4,18 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.databinding20210910.R
+import com.example.databinding20210910.databinding.FragmentsMyFriendsListBinding
 
 class MyFriendsListFragment : Fragment() {
 
+    lateinit var binding : FragmentsMyFriendsListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragments_my_friends_list, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragments_my_friends_list, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {

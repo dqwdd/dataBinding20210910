cdc9206ffff26cf0b1d320e30dce8fee3d67df74
dqwdd <dqwdd@naver.com>
2021-09-16 11:58:34 +0900

    친구 목록 표시 준비 중

---
+++ app/src/main/java/com/example/databinding20210910/ViewMyFriendsListActivity.kt
@@ -0,0 +1,19 @@
+package com.example.databinding20210910
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewMyFriendsListActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_my_friends_list)
+        setupEvent()
+        setValues()
+    }
+
+    override fun setupEvent() {
+    }
+
+    override fun setValues() {
+    }
+}

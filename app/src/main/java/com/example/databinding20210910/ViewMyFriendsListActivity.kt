b80b691141feaeeb3c5872260f684cc91fa1d844
dqwdd <dqwdd@naver.com>
2021-09-16 15:39:35 +0900

    커스텀액션바에서 물려준 +버튼 활용(editPlaceImg), 오류 해결함

--- app/src/main/java/com/example/databinding20210910/ViewMyFriendsListActivity.kt
+++ app/src/main/java/com/example/databinding20210910/ViewMyFriendsListActivity.kt
@@ -1,6 +1,8 @@
 package com.example.databinding20210910
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.example.databinding20210910.adapters.FriendPagerAdapter
 import com.example.databinding20210910.databinding.ActivityViewMyFriendsListBinding
@@ -18,11 +20,18 @@ class ViewMyFriendsListActivity : BaseActivity() {
     }
 
     override fun setupEvent() {
+
+        editPlaceImg.setOnClickListener {
+            val myIntent = Intent(mContext, AddFriendActivity::class.java)
+            startActivity(myIntent)
+        }
+
     }
 
     override fun setValues() {
 
         titleTxt.text = "친구"
+        editPlaceImg.visibility = View.VISIBLE
 
         mFPA = FriendPagerAdapter( supportFragmentManager )
         binding.friendsViewPager.adapter = mFPA

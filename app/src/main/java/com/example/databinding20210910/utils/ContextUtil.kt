fe00ec7cc41bc510c5860c065cc1454b21dbee69
dqwdd <dqwdd@naver.com>
2021-09-23 16:10:20 +0900

    로그인 한 사용자 저장 시 - 기기 자체에 준비 시간도 기록 - 앱이 종료된 상태에서 알람을 울릴 때도 문제 없도록

--- app/src/main/java/com/example/databinding20210910/utils/ContextUtil.kt
+++ app/src/main/java/com/example/databinding20210910/utils/ContextUtil.kt
@@ -13,6 +13,7 @@ class ContextUtil {
 //        여러 항목 저장 가능 => 항목 명들도 변수들로 저장
 //        -->일단 토큰
         val TOKEN = "TOKEN"
+        val MY_READY_MINUTE = "MY_READY_MINUTE"
 
 //        토큰 저장 / 조회
 
@@ -24,6 +25,18 @@ class ContextUtil {
         fun getToken(context: Context) : String {//메모장을 열어줘야하나context, token을 string으로 set
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
             return pref.getString(TOKEN, "")!!//스트링으로 내놔, TOKEN항목에 저장된걸, : String해놔서 널은 안되니 ""로 하자
+        }//
+
+
+
+        fun setMyReadyMinute(context: Context, minute: Int) {//메모장을 열어줘야하나context, token을 string으로 set
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putInt(MY_READY_MINUTE, minute).apply()
+        }
+
+        fun getMyReadyMinute(context: Context) : Int {//메모장을 열어줘야하나context, token을 string으로 set
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            return pref.getInt(MY_READY_MINUTE, 0)!!//스트링으로 내놔, TOKEN항목에 저장된걸, : String해놔서 널은 안되니 ""로 하자
         }//
 
     }

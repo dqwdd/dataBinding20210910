1b100e01a7ab9358f96b521214305361c6b3aaef
dqwdd <dqwdd@naver.com>
2021-09-23 12:14:14 +0900

    알람 울릴 때 => Notification을 울릴 채널만 우선 준비

--- app/src/main/java/com/example/databinding20210910/receivers/AlarmReceiver.kt
+++ app/src/main/java/com/example/databinding20210910/receivers/AlarmReceiver.kt
@@ -1,8 +1,13 @@
 package com.example.databinding20210910.receivers
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.util.Log
 
 class AlarmReceiver : BroadcastReceiver() {
@@ -10,15 +15,50 @@ class AlarmReceiver : BroadcastReceiver() {
     companion object {
 
         val ALARM_ID = 1001
+        val PRIMARY_CHANNEL_ID = "PRIMARY_CHANNEL_ID"
 
     }
 
+    //알람을 관리하는 클래스를 멤버변수로 선언
+    lateinit var mNotificationManager : NotificationManager
 
 
-    override fun onReceive(context: Context?, intent: Intent?) {
+    override fun onReceive(context: Context, intent: Intent?) {
 
         Log.d("알람 울림", "테스트로그")
+        mNotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        //알림 - Oreo버전 이후로는 채널을 선정해야 함. 그 이전 버전에서는 하면 안됨
+        createNotificationChannel()
+
+    }
+
+    fun createNotificationChannel() {
+        //폰에 깔린 OS가 오레오 버전 이상인가?
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            //맞을 때만 알림 채널 설정
+            val notiChannel = NotificationChannel(PRIMARY_CHANNEL_ID,
+                "Primary Channel",
+                NotificationManager.IMPORTANCE_HIGH)
+
+            notiChannel.enableLights(true)
+            notiChannel.lightColor = Color.RED
+
+            notiChannel.enableVibration(true)//진동
+            notiChannel.description = "알람을 통한 Notification 테스트"//설명 문구
+
+
+            //알림(Notification) 매니저를 통해서 채널을 등록해 줘야 함
+            mNotificationManager.createNotificationChannel(notiChannel)
+
+
+
+        }
 
 
     }
+
+
+
 }
--- app/src/main/java/com/example/databinding20210910/services/MyJobService.kt
+++ app/src/main/java/com/example/databinding20210910/services/MyJobService.kt
@@ -5,6 +5,7 @@ import android.app.PendingIntent
 import android.app.job.JobParameters
 import android.app.job.JobService
 import android.content.Intent
+import android.os.SystemClock
 import android.util.Log
 import com.example.databinding20210910.receivers.AlarmReceiver
 import java.util.*
@@ -44,7 +45,7 @@ class MyJobService : JobService() {
 
         //알람이 울릴 시간 설정 (임시로 1분 후로 설정 함)
         val now = Calendar.getInstance()//현재 시간 가져오기
-        val triggerTime = now.timeInMillis + 60 * 1000
+        val triggerTime = SystemClock.elapsedRealtime() + 60 * 1000
 
         //실제 알람 기능 설정
         alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerTime, pendingIntent)
